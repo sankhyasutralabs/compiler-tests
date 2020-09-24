99172d9425828b4a571fc2e50a70b671a038735b
Soumyadeep Bhattacharya <sbhtta@sankhyasutralabs.com>
2020-09-24 20:36:26 +0530

    refactor: remove MPI from source

--- scale.c
+++ scale.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
+#include <sys/time.h>
 
 #define STREAM_TYPE       double
 #define GRIDNX            3000
@@ -8,16 +8,17 @@
 #define STREAM_ARRAY_SIZE (GRIDNX * GRIDNY)
 #define NTIMES            10
 
+double
+wtime()
+{
+  struct timeval tp;
+  int i = gettimeofday(&tp, NULL);
+  return ((double)tp.tv_sec + (double)tp.tv_usec * 1e-6);
+}
+
 int
 main(int argc, char* argv[])
 {
-  MPI_Init(&argc, &argv);
-
-  int mpi_rank, mpi_size;
-  MPI_Comm mpi_comm = MPI_COMM_WORLD;
-  MPI_Comm_rank(mpi_comm, &mpi_rank);
-  MPI_Comm_size(mpi_comm, &mpi_size);
-
   static STREAM_TYPE a[STREAM_ARRAY_SIZE];
   static STREAM_TYPE b[STREAM_ARRAY_SIZE];
 
@@ -33,50 +34,43 @@ main(int argc, char* argv[])
 
   // 1D STREAM Scale kernel
   for (size_t t = 0; t < NTIMES; t++) {
-    start = MPI_Wtime();
+    start = wtime();
     for (size_t i = 0; i < STREAM_ARRAY_SIZE; i++)
     {
       a[i] = scalar * b[i];
     }
-    time1 += MPI_Wtime() - start;
+    time1 += wtime() - start;
   }
 
   // 2D STREAM Scale kernel
   for (size_t t = 0; t < NTIMES; t++) {
-    start = MPI_Wtime();
+    start = wtime();
     for (size_t y = 0; y < GRIDNY; y++)
     for (size_t x = 0; x < GRIDNX; x++)
     {
       size_t i = x + GRIDNX * y;
       a[i] = scalar * b[i];
     }
-    time2 += MPI_Wtime() - start;
+    time2 += wtime() - start;
   }
 
   // calculate maximum time per kernel call
   time1 /= (1. * NTIMES);
   time2 /= (1. * NTIMES);
-  double maxtime1, maxtime2;
-  MPI_Reduce(&time1, &maxtime1, 1, MPI_DOUBLE, MPI_MAX, 0, mpi_comm);
-  MPI_Reduce(&time2, &maxtime2, 1, MPI_DOUBLE, MPI_MAX, 0, mpi_comm);
 
   // print bandwidth
   size_t nops = 2;
   double bytes = sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE;
-  double bw1 = 1e-9 * nops * mpi_size * bytes / maxtime1;
-  double bw2 = 1e-9 * nops * mpi_size * bytes / maxtime2;
-  if (0 == mpi_rank) {
-    printf("scale 1D : %.2f GB/s, scale 2D : %.2f GB/s\n", bw1, bw2);
-  }
+  double bw1 = 1e-9 * nops * bytes / time1;
+  double bw2 = 1e-9 * nops * bytes / time2;
+  printf("scale 1D : %.2f GB/s, scale 2D : %.2f GB/s\n", bw1, bw2);
 
   // verify result
-   MPI_Barrier(mpi_comm);
   size_t errors = 0;
   for (size_t i = 0; i < STREAM_ARRAY_SIZE; i++)
   {
     if (a[i] != scalar) {
       printf("error: found error in results\n");
-      MPI_Abort(mpi_comm, 2);
       exit(1);
     }
   }
