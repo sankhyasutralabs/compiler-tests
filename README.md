71f16d7c5bcdc6453aa3c59565a0090181af548b
Soumyadeep Bhattacharya <sbhtta@sankhyasutralabs.com>
2021-03-01 01:30:51 +0530

    feat: add 3d scale kernel

--- README.md
+++ README.md
@@ -30,20 +30,20 @@ mpirun -n 16 --map-by numa:pe=8 ./scale.aocc
 ## Sample Output for `scale.icc`
 
 ```
-scale 1D : 23.46 GB/s, scale 2D : 21.63 GB/s
-scale 1D : 24.27 GB/s, scale 2D : 21.65 GB/s
-scale 1D : 23.48 GB/s, scale 2D : 21.70 GB/s
-scale 1D : 23.39 GB/s, scale 2D : 21.69 GB/s
-scale 1D : 21.61 GB/s, scale 2D : 25.47 GB/s
-scale 1D : 22.29 GB/s, scale 2D : 21.60 GB/s
-scale 1D : 22.78 GB/s, scale 2D : 21.69 GB/s
-scale 1D : 21.51 GB/s, scale 2D : 26.18 GB/s
-scale 1D : 22.71 GB/s, scale 2D : 21.57 GB/s
-scale 1D : 21.51 GB/s, scale 2D : 25.36 GB/s
-scale 1D : 21.53 GB/s, scale 2D : 25.29 GB/s
-scale 1D : 22.23 GB/s, scale 2D : 21.59 GB/s
-scale 1D : 21.53 GB/s, scale 2D : 24.08 GB/s
-scale 1D : 21.55 GB/s, scale 2D : 24.51 GB/s
-scale 1D : 21.48 GB/s, scale 2D : 24.50 GB/s
-scale 1D : 21.57 GB/s, scale 2D : 23.99 GB/s
+scale 1D : 22.21 GB/s, scale 2D : 21.81 GB/s, scale 3D : 21.82 GB/s
+scale 1D : 22.00 GB/s, scale 2D : 21.67 GB/s, scale 3D : 21.70 GB/s
+scale 1D : 22.16 GB/s, scale 2D : 21.64 GB/s, scale 3D : 21.69 GB/s
+scale 1D : 22.07 GB/s, scale 2D : 21.74 GB/s, scale 3D : 21.76 GB/s
+scale 1D : 22.32 GB/s, scale 2D : 21.80 GB/s, scale 3D : 21.70 GB/s
+scale 1D : 22.14 GB/s, scale 2D : 21.59 GB/s, scale 3D : 21.65 GB/s
+scale 1D : 22.13 GB/s, scale 2D : 21.61 GB/s, scale 3D : 21.58 GB/s
+scale 1D : 21.56 GB/s, scale 2D : 21.61 GB/s, scale 3D : 23.51 GB/s
+scale 1D : 22.07 GB/s, scale 2D : 21.59 GB/s, scale 3D : 21.58 GB/s
+scale 1D : 21.63 GB/s, scale 2D : 21.71 GB/s, scale 3D : 23.27 GB/s
+scale 1D : 21.48 GB/s, scale 2D : 21.52 GB/s, scale 3D : 23.19 GB/s
+scale 1D : 21.64 GB/s, scale 2D : 21.72 GB/s, scale 3D : 23.65 GB/s
+scale 1D : 21.74 GB/s, scale 2D : 21.72 GB/s, scale 3D : 23.35 GB/s
+scale 1D : 21.66 GB/s, scale 2D : 21.73 GB/s, scale 3D : 23.25 GB/s
+scale 1D : 21.57 GB/s, scale 2D : 21.62 GB/s, scale 3D : 23.65 GB/s
+scale 1D : 21.57 GB/s, scale 2D : 21.60 GB/s, scale 3D : 23.42 GB/s
 ```
--- scale.c
+++ scale.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-#define STREAM_TYPE       double
-#define GRIDNX            3000
-#define GRIDNY            GRIDNX
-#define STREAM_ARRAY_SIZE (GRIDNX * GRIDNY)
 #define NTIMES            10
+#define STREAM_TYPE       double
+#define NA                16
+#define GRIDNX3D          (NA*NA)
+#define GRIDNY3D          GRIDNX3D
+#define GRIDNZ3D          GRIDNX3D
+#define GRIDNX2D          (NA*NA*NA)
+#define GRIDNY2D          GRIDNX2D
+#define GRIDNX1D          (NA*NA*NA*NA*NA*NA)
+#define STREAM_ARRAY_SIZE GRIDNX1D
 
 double
 wtime()
@@ -30,13 +35,14 @@ main(int argc, char* argv[])
     b[i] = 1.0;
   }
 
-  double start, time1 = 0, time2 = 0;
+  double start, time1 = 0, time2 = 0, time3 = 0;
 
   // 1D STREAM Scale kernel
   for (size_t t = 0; t < NTIMES; t++) {
     start = wtime();
-    for (size_t i = 0; i < STREAM_ARRAY_SIZE; i++)
+    for (size_t x = 0; x < GRIDNX1D; x++)
     {
+      size_t i = x;
       a[i] = scalar * b[i];
     }
     time1 += wtime() - start;
@@ -45,25 +51,40 @@ main(int argc, char* argv[])
   // 2D STREAM Scale kernel
   for (size_t t = 0; t < NTIMES; t++) {
     start = wtime();
-    for (size_t y = 0; y < GRIDNY; y++)
-    for (size_t x = 0; x < GRIDNX; x++)
+    for (size_t y = 0; y < GRIDNY2D; y++)
+    for (size_t x = 0; x < GRIDNX2D; x++)
     {
-      size_t i = x + GRIDNX * y;
+      size_t i = x + GRIDNX2D * y;
       a[i] = scalar * b[i];
     }
     time2 += wtime() - start;
   }
 
+  // 3D STREAM Scale kernel
+  for (size_t t = 0; t < NTIMES; t++) {
+    start = wtime();
+    for (size_t z = 0; z < GRIDNZ3D; z++)
+    for (size_t y = 0; y < GRIDNY3D; y++)
+    for (size_t x = 0; x < GRIDNX3D; x++)
+    {
+      size_t i = x + GRIDNX3D * y + GRIDNX3D * GRIDNY3D * z;
+      a[i] = scalar * b[i];
+    }
+    time3 += wtime() - start;
+  }
+
   // calculate maximum time per kernel call
   time1 /= (1. * NTIMES);
   time2 /= (1. * NTIMES);
+  time3 /= (1. * NTIMES);
 
   // print bandwidth
   size_t nops = 2;
   double bytes = sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE;
   double bw1 = 1e-9 * nops * bytes / time1;
   double bw2 = 1e-9 * nops * bytes / time2;
-  printf("scale 1D : %.2f GB/s, scale 2D : %.2f GB/s\n", bw1, bw2);
+  double bw3 = 1e-9 * nops * bytes / time3;
+  printf("scale 1D : %.2f GB/s, scale 2D : %.2f GB/s, scale 3D : %.2f GB/s\n", bw1, bw2, bw3);
 
   // verify result
   size_t errors = 0;
@@ -77,3 +98,4 @@ main(int argc, char* argv[])
 
   return 0;
 }
+~
